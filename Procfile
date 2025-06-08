ce64d2bfafe11699a5588f6359b149bf8afa7822
AdityaJare <adityajare2004@gmail.com>
2025-06-08 14:25:47 +0530

    Refactor build script and remove unused Vercel configuration; add Procfile for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn multibpreneur_website.wsgi:application
--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
 
-# Install dependencies
+# Install Python dependencies
 pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput
+python manage.py collectstatic --no-input
 
-# Make migrations
-python manage.py makemigrations
-
-# Apply migrations
+# Run migrations
 python manage.py migrate
